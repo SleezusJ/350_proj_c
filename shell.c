d4cd83ba3549ff6d0a600b3d43a3e857adeece26
Kyle <k1snow@student.bridgew.edu>
2019-11-22 00:39:00 -0500

    IT ALL WORKS
    So why does it feel so hacky??

--- shell.c
+++ shell.c
@@ -1,13 +1,18 @@
 
 void type(char* filename);
 int strCompare(char* str1, char* str2);
+void getcmdName(char* cmdbuffer, char* cmdName, int* spaceIndex);
+void getfileName(char* cmdbuffer, char* filename, int spaceIndex);
 
 int main(){
 
-char cmdBuffer[11]; //max cmd len + max fiename len + 1(space)
-char cmdName[4];
-char filename[6];
+char cmdBuffer[20]; 
+char* cmdName;  //[4]
+char filename[6];  //[6]
+int spi; //index of SPACE in command buffer(used to differentiate between cmd & filename)
 int i;
+
+
 	while(1){
 
 		//print prompt:
@@ -17,27 +22,47 @@ int i;
 
 
 		//parse out command name
-		for(i=0;i<4;i++){cmdName[i] = cmdBuffer[i];}
+		//for(i=0;i<4;i++){cmdName[i] = cmdBuffer[i];}
+		//cmdName[4]=0x0;  //add NULL terminator
+		getcmdName(cmdBuffer, cmdName, &spi);
+
+		//syscall(0,"command buffer: \r\n");
+		//syscall(0,cmdBuffer); 
+
+		syscall(0,"cmdName: \r\n");
+		syscall(0,cmdName); syscall(0,"\r\n");
+
+		//parse out filename
+		for(i=0;i<6;i++){filename[i] = cmdBuffer[i+5];}	
+		filename[6]=0x0;
+		//getfileName(cmdBuffer, filename, &spi);		
 		
+		syscall(0,"file name: \r\n");
+		syscall(0,filename); syscall(0,"\r\n");
+		
+
+
 		//compare cmd
 		if(strCompare(cmdName, "type") == 1){ //if command is type.
 			
 			//load file name
-			for(i=0;i<6;i++){filename[i] = cmdBuffer[i+5];}
+			//for(i=0;i<6;i++){filename[i] = cmdBuffer[i+5];}
 			 
 			//print contents of file designated by 6 char long filename
 			type(filename);
 	
-		}else if (strCompare(cmdName, "exec")==1){ //if command is exec.
+		}else if (strCompare("exec", cmdName)==1){ //if command is exec.
 
-			for(i=0;i<6;i++){filename[i] = cmdBuffer[i+5];}
+			//for(i=0;i<6;i++){filename[i] = cmdBuffer[i+5];}
 		
 			//try to execute program designated by 6 char long filename
 			syscall(4, filename);
 
 		}else{
-			syscall(0, "Command not found!\n\r");
+			syscall(0, "Command not found!\r\n");
+			
 		}
+
 
 	}
 
@@ -49,13 +74,13 @@ void type(char* filename){
 	char buffer[13312]; //max file size
 	int readSectors;
 	int i;
-	//int offset = 0x0;
 	
 	syscall(3,filename,buffer,&readSectors); //read file into buffer
 	if(readSectors>0){
 		syscall(0,buffer); //print buffer
+		syscall(0,"\r\n");//-----------
 		return;
-	}else{syscall(0,"File not found!"); return;}
+	}else{syscall(0,"File not found!\r\n"); return;}
 
 
 }
@@ -74,6 +99,37 @@ int strCompare(char* str1, char* str2){
 	}
 	return 1;
 }
+
+//takes incoming command buffer, empty buffer to write command to, and int address to write index of SPACE to.
+//
+void getcmdName(char* cmdbuffer, char* cmdName, int* spaceIndex){
+
+	int i = 0;
+	while(cmdbuffer[i]!=0x20){
+		*cmdName = cmdbuffer[i];
+		i++;
+		cmdName++;
+	}
+	*cmdName = 0x0; //NULL terminate string
+	*spaceIndex = *spaceIndex+i; //reference to location of SPACE in cmdbuffer
+	return;
+}
+
+void getfileName(char* cmdbuffer, char* filename, int* spaceIndex){
+	
+	int i = *spaceIndex + 1; //location of first element after space
+	while(cmdbuffer[i] != 0xa){ //
+		*filename = cmdbuffer[i];
+		i++;			
+		filename++;	
+	}
+	*filename = 0x0; //null terminate string
+	return;
+}
+
+
+
+
 
 
 
